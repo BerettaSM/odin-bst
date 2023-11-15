e8bf418751cc1bfc28517cf02e322c92ebc6957b
BerettaSM <ramonsaviato@hotmail.com>
2023-11-14 23:15:03 -0300

    feat: implement build_tree method

--- bst.rb
+++ bst.rb
@@ -17,6 +17,18 @@ class Tree
     attr_accessor :root
 
     def initialize(array)
-        @root = nil
+        treated_array = array.sort.uniq
+        @root = build_tree(treated_array)
+    end
+
+    private
+
+    def build_tree(array)
+        return nil if array.nil? || array.empty?
+        mid = array.length / 2
+        root = TreeNode.new(array[mid])
+        root.left = build_tree(array[0, mid])
+        root.right = build_tree(array[mid+1..])
+        return root
     end
 end
