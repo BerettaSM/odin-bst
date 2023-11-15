435dc9c45cf467311a5e49526c3eeb027498b308
BerettaSM <ramonsaviato@hotmail.com>
2023-11-14 22:56:45 -0300

    feat: add tree node class

---
+++ bst.rb
@@ -0,0 +1,9 @@
+class TreeNode
+    attr_accessor :data, :left, :right
+
+    def initialize(data, left=nil, right=nil)
+        @data = data
+        @left = left
+        @right = right
+    end
+end
